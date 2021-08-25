32f70379206a87e9189887580534c31d3ebe1367
Tyler <tylershelton99@gmail.com>
2021-08-25 21:28:06 +0000

    comment code and refactor to modularize code to pull out setting the beeper

--- timer1.js
+++ timer1.js
@@ -1,21 +1,23 @@
+// create an empty array that will store the alarms
 const alarms = [];
-const alarmsToSet = [];
-
+// Place all of the timers that were provided via command line arguments into the alarms array
 process.argv.slice(2).forEach(element => alarms.push(Number(element)));
 
+// function that will set a beep alarm for the time given in seconds
+const alarmBeep = timer => {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, timer * 1000);
+};
+
+// If no timers are passed, return an empty array
 if (alarms.length === 0) {
   return;
 }
 
+// loop through the alarms array and set a beep for valid numbers > 0
 alarms.forEach(alarm => {
   if (typeof alarm === "number" && alarm > 0) {
-    alarmsToSet.push(alarm);
+    alarmBeep(alarm);
   }
-});
-alarmsToSet.sort((a, b) => a - b);
-
-alarmsToSet.forEach(alarm => {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, alarm * 1000);
 });
